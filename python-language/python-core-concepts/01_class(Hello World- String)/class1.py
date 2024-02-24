af699e0771507cf20876b8fea71c5a3b564c7cc0
Chaudhry Wasam Ur Rehman <140172148+ChWasam@users.noreply.github.com>
2024-02-24 17:38:10 +0500

    Created complete setup for code

---
+++ python-language/python-core-concepts/01_class(Hello World- String)/class1.py
@@ -0,0 +1,13 @@
+print("wasam")
+
+# name : str = 700  # Getting error which means mypy extention is active 
+
+# print(name)S
+
+
+# to see error in line 3 you can also type " mypy class1.py " in terminal\
+# In terminal 
+#mypy class1.py 
+# Result 
+# error: Incompatible types in assignment (expression has type "int", variable has type "str")  [assignment]
+# Found 1 error in 1 file (checked 1 source file)
