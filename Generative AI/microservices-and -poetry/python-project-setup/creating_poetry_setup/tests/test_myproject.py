84c0a2d592f9ad956ffd4fc879d87ebc2efb9094
ChWasam <wasam.developer@gmail.com>
2024-03-24 15:48:11 +0500

    Poetry Setup: Few more changes

---
+++ Generative AI/microservices-and -poetry/python-project-setup/creating_poetry_setup/tests/test_myproject.py
@@ -0,0 +1,9 @@
+from creating_poetry_setup import main
+
+def test_function1():
+    r = main.my_first_function()
+    assert r == "Hello World"
+def test_function2():
+    r = main.my_first_function()
+    assert r != "Hello Worldddd"
+
