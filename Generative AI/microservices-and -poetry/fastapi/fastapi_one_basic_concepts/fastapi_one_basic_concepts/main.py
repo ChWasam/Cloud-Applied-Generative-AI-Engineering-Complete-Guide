49f3ecec1869c7a7d7e95bb5f4e43f7dcf4c55eb
ChWasam <wasam.developer@gmail.com>
2024-04-21 23:43:16 +0500

    First exaple related to path parameter

--- Generative AI/microservices-and -poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/main.py
+++ Generative AI/microservices-and -poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def greet():
     return "Hello World"
+
+# path parameter API
+
+@app.get("/books/{book_name}")
+def get_book_info(book_name:str):
+    return {"message":f"you requested information about {book_name}"}
+
