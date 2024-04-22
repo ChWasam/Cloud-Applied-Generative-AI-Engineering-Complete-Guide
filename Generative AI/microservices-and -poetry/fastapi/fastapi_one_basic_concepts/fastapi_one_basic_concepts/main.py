635921eaf5fab09e029b456e104ddf685610f08b
ChWasam <wasam.developer@gmail.com>
2024-04-22 15:04:09 +0500

    Query Parameters Practice

--- Generative AI/microservices-and -poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/main.py
+++ Generative AI/microservices-and -poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/main.py
@@ -35,6 +35,79 @@ def product_info(product_name:str,product_id:int):
     }
  
 
+#  Query Parameters 
+
+# starts with ?
+# contains key value pair
+# Seperated by &
+
+# @app.get("/items")
+# def items(q:int):
+#     return {"q" : q}
+
+# we can set by default value 
+
+# @app.get("/items")
+# def items(q:int = 1):
+#     return {"q" : q}
+
+# @app.get("/items")
+# def items(q:int =12,skip:int = 0):
+#     return {"q" : q,
+#             "skip": skip}
+
+# Use Case pagination
+
+dumy_data = [{"item_name":"cake" },{"item_name":"Cake Rusk"},{"item_name":"cookies"}]
+
+@app.get("/items")
+def get_items(skip:int=0, limit:int=10):
+    return dumy_data[skip:skip+limit]
+
+@app.get("/books/{book_name}")
+def books(book_name:str,q:str|None = None):
+    if q:
+        return{"book_name":book_name, "q":q }
+    return{"book_name":book_name}
+
+@app.get("/subjects/{subject_id}")
+def subject(subject_id:str,q:str|None = None, short:bool = False):
+    subject = {"subject_id":subject_id
+               ,"description": "Book is Long"}
+    if q:
+        subject.update({"q":q})
+    if short:
+        subject.update({"description": "Book is short"})
+
+    return subject
+
+#  we can use True or true or on or yes
+# @app get ("users/{user_id}/items/{item_id}")
+# async def read_user_item(
+# user_id: int, item_id: str, q: str | None = None, short: bool = False
+
+
+    
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
