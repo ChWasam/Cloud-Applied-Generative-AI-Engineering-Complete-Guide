a67ac67d94e6c8e4992bfa5f967de652c5878b01
ChWasam <wasam.developer@gmail.com>
2024-05-06 00:23:37 +0500

    Basic response completed

--- Generative AI/microservices-and-poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/request.py
+++ Generative AI/microservices-and-poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/request.py
@@ -1,15 +1,49 @@
-import requests
-# from requests.models import Response
-# from requests.structures import CaseInsensitiveDict
+# Import both pakages requests and types-requests
 
+
+
+import requests
+from requests.models import Response
+from requests.structures import CaseInsensitiveDict
 from fastapi import FastAPI
 
-response: str  = requests.get("https://simple-books-api.glitch.me")
-print(response)
+response: Response  = requests.get("https://simple-books-api.glitch.me")
+
+json_obj: dict = response.json()
+
+status_code:int = response.status_code
+
+text :str = response.text
+
+headers : CaseInsensitiveDict = response.headers
 
 
-app1 =  FastAPI()
-@app1.get("/response")
+
+
+
+
+# print(type(request))
+
+
+app =  FastAPI()
+@app.get("/response")
 def Urlresponse():
-    return response
+    return json_obj
+#  yahan agar response ko direct return karain  ga to nahi ho ga 
+#  json me convert karna zarori ha 
+
+@app.get("/status_code")
+def successfull ():
+    return status_code
+
+@app.get("/text")
+def text_response ():
+    return text
+
+@app.get("/headers")
+def headers_response ():
+    return headers
+
+
+
 
