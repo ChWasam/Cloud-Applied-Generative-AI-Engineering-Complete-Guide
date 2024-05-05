cfa0702438f0f794c242cd2054ba05551b627c94
ChWasam <wasam.developer@gmail.com>
2024-05-05 23:51:54 +0500

    fastapi, Request

---
+++ Generative AI/microservices-and-poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/request.py
@@ -0,0 +1,14 @@
+import requests
+from requests.models import Response
+from requests.structures import CaseInsensitiveDict
+from fastapi import FastAPI
+
+response: str  = requests.get("https://simple-books-api.glitch.me")
+print(response)
+
+
+app1 =  FastAPI()
+@app1.get("/response")
+def Urlresponse():
+    return response
+
