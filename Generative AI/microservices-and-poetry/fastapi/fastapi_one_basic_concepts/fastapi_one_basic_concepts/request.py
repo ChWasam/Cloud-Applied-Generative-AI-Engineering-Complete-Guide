36f461a46f98f9acef3c5707c0771af0f269a7f7
ChWasam <wasam.developer@gmail.com>
2024-05-06 12:32:13 +0500

    request update

--- Generative AI/microservices-and-poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/request.py
+++ Generative AI/microservices-and-poetry/fastapi/fastapi_one_basic_concepts/fastapi_one_basic_concepts/request.py
@@ -7,19 +7,42 @@ from requests.models import Response
 from requests.structures import CaseInsensitiveDict
 from fastapi import FastAPI
 
-response: Response  = requests.get("https://simple-books-api.glitch.me")
+# 1
+# response: Response  = requests.get("https://simple-books-api.glitch.me")
+# 2
+# response: Response  = requests.get("https://simple-books-api.glitch.me/books")
+# 3
+# response: Response  = requests.get("https://simple-books-api.glitch.me/books/3")
+# 4 query parameters
+# response: Response  = requests.get("https://simple-books-api.glitch.me/books?type=fiction&&limit=2")
+# 5
+response: Response  = requests.get("https://simple-books-api.glitch.me/books/3")
+
+
+#  Jo bhi secret info hoti ha wo client sa header ka zariya server ko bhajain ga 
+#  header me jo bhi info bhajain ga wo encrypt ho ge 
+#  Encrypt hona ka bad server pa jati ha 
+# aur phir server  pa jana ka bad decrypt hoti ha 
+
+
 
 json_obj: dict = response.json()
 
 status_code:int = response.status_code
 
 text :str = response.text
+#  Agar json me response na ho to phir text sa xlient pa show kar sakta han 
 
 headers : CaseInsensitiveDict = response.headers
 # yeh jo header ha yeh server ka response ka ha 
 #  server ka aupar yeh time hua ha 
 # server ke tamam info
 #  client sa bhi ak header ke info jati ha e.g browser konsa ha aur ip address konsa ha
+
+# process and method of encryption 
+
+# Authorization: Bearer <YOUR TOKEN>
+# headers : dict = {"Authorization" : "Bearer e232c09f3d1fcb27eac378535450bfd1ac489b607390938ec395b7aa1d5d151f"}
 
 
 
@@ -30,7 +53,7 @@ headers : CaseInsensitiveDict = response.headers
 
 
 app =  FastAPI()
-@app.get("/response")
+@app.get("/json_response")
 def Urlresponse():
     return json_obj
 #  yahan agar response ko direct return karain  ga to nahi ho ga 
