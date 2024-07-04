ac566fdd870f7458ac0a5077f79662227b2a4f06
ChWasam <wasam.developer@gmail.com>
2024-07-04 16:40:23 +0500

    Renumbering

---
+++ Cloud_Generative_AI/cloud_native_computing/projects/complete_product_service_clean_code_05/product_service/app/consumer/main.py
@@ -0,0 +1,339 @@
+from sqlmodel import SQLModel,Field,create_engine,select, Session
+from fastapi import FastAPI,Depends,HTTPException
+from app import settings
+from contextlib import asynccontextmanager
+from aiokafka import AIOKafkaConsumer,AIOKafkaProducer
+from typing import Annotated 
+from app import product_pb2
+from app import settings
+import asyncio
+import logging
+import psycopg
+import uuid
+from uuid import UUID
+import logging
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Retry utility
+async def retry_async(func, retries=5, delay=2, *args, **kwargs):
+    for attempt in range(retries):
+        try:
+            print("retry")
+            return await func(*args, **kwargs)
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            if attempt < retries - 1:
+                await asyncio.sleep(delay)
+            else:
+                raise
+
+# Configure the logger 
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+class Product(SQLModel, table=True):
+    id : int|None = Field(default = None , primary_key= True)
+    product_id:UUID = Field(default_factory=uuid.uuid4, index=True)
+    name:str = Field(index=True)
+    description:str = Field(index=True)
+    price:float = Field(index=True)
+    is_available: bool = Field(default=True)
+
+
+connection_string:str = str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
+
+engine = create_engine(connection_string , pool_recycle=300 , pool_size=10 , echo=True)
+
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+# def get_session():
+#     with Session(engine) as session:
+#      yield session
+
+
+# @asynccontextmanager
+# async def lifespan():   
+#    create_table()
+#    loop = asyncio.get_event_loop()
+#    task = loop.create_task(consume_message())
+#    yield
+#    task.cancel()
+#    await task
+
+
+# writing Consumer code 
+async def consume_message_request():
+    consumer = AIOKafkaConsumer(
+        f"{settings.KAFKA_TOPIC}",
+        bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}",
+        group_id= f"{settings.KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT}",
+        auto_offset_reset='earliest'
+    )
+    await retry_async(consumer.start)
+    try:
+        async for msg in consumer:
+            print(f"message from consumer : {msg}")
+            try:
+                new_msg = product_pb2.Product()
+                new_msg.ParseFromString(msg.value)
+                logger.info(f"new_msg:{new_msg}")
+                if new_msg.option == product_pb2.SelectOption.GET_ALL:
+                    with Session(engine) as session:
+                        products_list_from_db = session.exec(select(Product)).all()
+                        logger.info(f"List of products from database:{products_list_from_db} ")
+                        product_list_proto = product_pb2.ProductList()
+                        for product in products_list_from_db:
+                            product_proto = product_pb2.Product(
+                                id=product.id,
+                                product_id=str(product.product_id),
+                                name=product.name,
+                                description=product.description,
+                                price=product.price,
+                                is_available=product.is_available,
+                            )
+                            product_list_proto.products.append(product_proto)
+                        logger.info(f"List of products in proto :{product_list_proto} ")
+                        producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                        await retry_async(producer.start)
+                        try:
+                            serialized_product_list = product_list_proto.SerializeToString()
+                            await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product_list)
+                        finally:
+                            await producer.stop()
+                        logger.info(f"List of products sent back from database: {product_list_proto} ")
+                elif new_msg.option == product_pb2.SelectOption.GET:
+                    msg_to_db = Product(product_id = new_msg.product_id)
+                    with Session(engine) as session:
+                        product = session.exec(select(Product).where(Product.product_id == msg_to_db.product_id)).first()
+                        if product:
+                            logger.info(f"product from database:{product}")
+                            product_proto = product_pb2.Product(
+                                id=product.id,
+                                product_id=str(product.product_id),
+                                name=product.name,
+                                description=product.description,
+                                price=product.price,
+                                is_available=product.is_available,
+                            )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+                            logger.info(f"product sent back from database: {product_proto} ")
+                        else:
+                            product_proto = product_pb2.Product(
+                                error_message = f"No Product with product_id :{msg_to_db.product_id} is found !",
+                                status = 400
+                                )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+
+                elif new_msg.option == product_pb2.SelectOption.CREATE:
+                    msg_to_db = Product(name = new_msg.name,description = new_msg.description, price = new_msg.price , is_available = new_msg.is_available )
+                    logger.info(f"msg_to_db:{msg_to_db}")
+                    with Session(engine) as session:
+                        session.add(msg_to_db)
+                        session.commit()
+                        logger.info(f"Product Added to database:{msg_to_db} ")
+
+
+
+                elif new_msg.option == product_pb2.SelectOption.UPDATE:
+                    msg_to_db = Product(product_id = new_msg.product_id, name = new_msg.name, description = new_msg.description, price = new_msg.price , is_available = new_msg.is_available)
+                    with Session(engine) as session:
+                        current_product = session.exec(select(Product).where(Product.product_id == msg_to_db.product_id)).first()
+                        if current_product:
+                            current_product.name = msg_to_db.name
+                            current_product.description = msg_to_db.description
+                            current_product.price = msg_to_db.price
+                            current_product.is_available = msg_to_db.is_available
+                            session.add(current_product)
+                            session.commit()
+                            session.refresh(current_product)
+                            logger.info(f"Product Updated in database:{msg_to_db} ")
+                            logger.info(f"product from database:{current_product}")
+                            product_proto = product_pb2.Product(
+                                id=current_product.id,
+                                product_id=str(current_product.product_id),
+                                name=current_product.name,
+                                description=current_product.description,
+                                price=current_product.price,
+                                is_available=current_product.is_available,
+                            )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+                            logger.info(f"product sent back from database: {product_proto} ")
+                        else:
+                            product_proto = product_pb2.Product(
+                                error_message = f"No Product with product_id :{msg_to_db.product_id} is found !",
+                                status = 400
+                                )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        # else:
+                        #     product_proto = product_pb2.Product(
+                        #         error_message = f"No Product with product_id :{msg_to_db.product_id} is found !",
+                        #         status = 400
+                        #     )
+                        #     logger.info(f"Product in proto :{product_proto} ")
+                        #     producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                        #     await retry_async(producer.start)
+                        #     try:
+                        #         serialized_product = product_proto.SerializeToString()
+                        #         await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                        #     finally:
+                        #         await producer.stop()
+                        #     logger.info(f"No Product with product_id :{msg_to_db.product_id} is  found !")
+
+                elif new_msg.option == product_pb2.SelectOption.DELETE:
+                    msg_to_db = Product(product_id = new_msg.product_id)
+                    with Session(engine) as session:
+                        current_product = session.exec(select(Product).where(Product.product_id == msg_to_db.product_id)).first()
+                        if current_product:
+                            logger.info(f"current_product :{current_product} is  found ! ")
+                            session.delete(current_product)
+                            session.commit()
+                            product_proto = product_pb2.Product(
+                                error_message = f"Product with product_id :{msg_to_db.product_id} deleted !",
+                                status = 200
+                            )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+                            logger.info(f"product sent back from database: {product_proto} ")
+                        else:
+                            product_proto = product_pb2.Product(
+                                error_message = f"No Product with product_id :{msg_to_db.product_id} is found !",
+                                status = 400
+                                )
+                            logger.info(f"Product in proto :{product_proto} ")
+                            producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+                            await retry_async(producer.start)
+                            try:
+                                serialized_product = product_proto.SerializeToString()
+                                await producer.send_and_wait(f"{settings.KAFKA_TOPIC_GET}", serialized_product)
+                            finally:
+                                await producer.stop()
+
+
+
+
+
+
+
+
+
+                        # else:
+                        #     # raise HTTPException(status_code=400, detail=f"No Product with id :{id} is found !")
+                        #     logger.info(f"No Product with id :{msg_to_db.product_id} is  found ! ")
+
+
+
+            except Exception as e:
+                logger.error(f"Error Processing Message: {e} ")    
+    finally:
+        await consumer.stop()
+
+
+
+
+# app:FastAPI = FastAPI(lifespan=lifespan)
+# @app.get("/")
+# async def read_root():
+#     return {"Hello":"Consumer Service to update Database"}
+
+
+
+
+# @app.post("/products", response_model = Product)
+# async def add_product(product:Product, session:Annotated[Session,Depends(get_session)]):
+#    session.add(product)
+#    session.commit()
+#    session.refresh(product)
+#    return product
+#     # Keep in mind that from ui we wont allow user to give primary key. It will be generated by postgres. If we will also be allowed to give it manually then there is a chance that we may find conflict as the id might already exist
+
+# @app.get("/products", response_model=list[Product])
+# async def get_all_products(session:Annotated[Session,Depends(get_session)]):
+#     products = session.exec(select(Product)).all()
+#     return products
+
+
+# @app.get("/products/{id}", response_model=Product)
+# async def get_a_product(id:int, session:Annotated[Session,Depends(get_session)]):
+#     product = session.exec(select(Product).where(Product.id == id)).first()
+#     if product:
+#         return product
+#     else:
+#         raise HTTPException(status_code=400, detail=f"No Product with id :{id} is found !")
+    
+# @app.put("/products/{id}",response_model=Product)
+# async def update_a_product(id:int, product: Product, session:Annotated[Session,Depends(get_session)]):
+#     current_product = session.exec(select(Product).where(Product.id == id)).first()
+#     if current_product:
+#         current_product.name = product.name
+#         current_product.price = product.price
+#         current_product.is_available = product.is_available
+#         session.add(current_product)
+#         session.commit()
+#         session.refresh(current_product)
+#         return(current_product)
+#     else:
+#         raise HTTPException(status_code=400,detail=f"No Product with id :{id} is found !")
+
+# @app.delete("/products/{id}",response_model=dict)
+# async def del_a_product(id:int , session:Annotated[Session,Depends(get_session)]):
+#     product = session.exec(select(Product).where(Product.id == id)).first()
+#     if product:
+#         session.delete(product)
+#         session.commit()
+#         return {f"{id}":"Deleted"}
+#     else:
+#         raise HTTPException(status_code=400, detail=f"No Product with id :{id} is found !")
+
+
---
+++ Cloud_Generative_AI/cloud_native_computing/projects/complete_product_service_clean_code_05/product_service/app/producer/main.py
@@ -0,0 +1,236 @@
+from sqlmodel import SQLModel,Field,create_engine,select, Session
+from google.protobuf.json_format import MessageToDict
+from fastapi import FastAPI,Depends,HTTPException
+from app import settings
+from contextlib import asynccontextmanager
+from typing import Annotated
+from aiokafka import AIOKafkaProducer , AIOKafkaConsumer
+from aiokafka.admin import AIOKafkaAdminClient,NewTopic
+from app import product_pb2
+from app import settings
+from app.consumer import main
+import uuid 
+from uuid import UUID 
+import asyncio
+import psycopg
+import logging
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Retry utility
+async def retry_async(func, retries=5, delay=2, *args, **kwargs):
+    for attempt in range(retries):
+        try:
+            return await func(*args, **kwargs)
+        except Exception as e:
+            logger.error(f"Attempt {attempt + 1} failed: {e}")
+            if attempt < retries - 1:
+                await asyncio.sleep(delay)
+            else:
+                raise
+
+# If the current attempt is the last one (else), the raise statement is executed.
+# The raise statement re-raises the caught exception e.
+# This means that after all retry attempts are exhausted, the function will raise the last encountered exception, propagating it to the caller.
+
+
+# Creating topic from code 
+
+async def create_topic ():
+    admin_client = AIOKafkaAdminClient(
+        bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}"
+    )
+    await retry_async(admin_client.start)
+    topic_list = [
+        NewTopic(name=f"{settings.KAFKA_TOPIC}", num_partitions=2, replication_factor=1),
+        NewTopic(name=f"{settings.KAFKA_TOPIC_GET}", num_partitions=2, replication_factor=1)
+    ]
+    try:
+        await admin_client.create_topics(new_topics=topic_list, validate_only= False)
+    except Exception as e:
+        logger.error ( "Error creating topics:: {e}")
+    finally:
+        await admin_client.close()
+
+
+
+async def consume_message_response_get_all():
+    consumer = AIOKafkaConsumer(
+    f"{settings.KAFKA_TOPIC_GET}",
+    bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}",
+    group_id= f"{settings.KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT_GET}",
+    auto_offset_reset='earliest'
+    )
+    await retry_async(consumer.start)
+    try:
+        async for msg in consumer:
+            logger.info(f"message from consumer : {msg}")
+            try:
+                new_msg = product_pb2.ProductList()
+                new_msg.ParseFromString(msg.value)
+                logger.info(f"new_msg on producer side:{new_msg}")
+                return new_msg
+            except Exception as e:
+                logger.error(f"Error Processing Message: {e} ")    
+    finally:
+        await consumer.stop()
+
+async def consume_message_response_get():
+    consumer = AIOKafkaConsumer(
+    f"{settings.KAFKA_TOPIC_GET}",
+    bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}",
+    group_id= f"{settings.KAFKA_CONSUMER_GROUP_ID_FOR_PRODUCT_GET}",
+    auto_offset_reset='earliest'
+    )
+    await retry_async(consumer.start)
+    try:
+        async for msg in consumer:
+            logger.info(f"message from consumer in producer  : {msg}")
+            try:
+                new_msg = product_pb2.Product()
+                new_msg.ParseFromString(msg.value)
+                logger.info(f"new_msg on producer side:{new_msg}")
+                return new_msg
+            except Exception as e:
+                logger.error(f"Error Processing Message: {e} ")    
+    finally:
+        await consumer.stop()
+
+
+class Product(SQLModel):
+    id : int|None = Field(default = None , primary_key= True)
+    product_id:UUID = Field(default_factory=uuid.uuid4, index=True)
+    name:str = Field(index=True)
+    description:str = Field(index=True)
+    price:float= Field(index=True)
+    is_available: bool = Field(default=True)
+
+async def produce_message():
+    producer = AIOKafkaProducer(bootstrap_servers= f"{settings.BOOTSTRAP_SERVER}")
+    await retry_async(producer.start)
+    try:
+        yield producer
+    finally:
+        await producer.stop()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    main.create_table()
+    await create_topic()
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(main.consume_message_request())
+    try:
+        yield
+    finally:
+        task.cancel()
+        await task
+
+
+# task.cancel(): This cancels the task that was created to consume messages.
+# await task: This waits for the task to complete its cancellation.
+# preferable to ensure proper task management and resource cleanup
+
+
+#  Focus on the error given below 
+# @asynccontextmanager
+# async def lifespan(app:FastAPI):
+#     main.create_table()
+#     await create_topic()
+#     asyncio.run(main.consume_message())  #  ERROR : asyncio.run() cannot be called from a running event loop
+#     yield
+#  ERROR : asyncio.run() cannot be called from a running event loop
+
+
+
+
+app:FastAPI = FastAPI(lifespan=lifespan )
+@app.get("/")
+async def read_root():
+    return {"Hello":"Product Service"}
+
+
+@app.get("/products", response_model= list[Product])
+async def get_all_products(producer:Annotated[AIOKafkaProducer,Depends(produce_message)]):
+    product_proto = product_pb2.Product(option = product_pb2.SelectOption.GET_ALL)
+    serialized_product = product_proto.SerializeToString()
+    await producer.send_and_wait(f"{settings.KAFKA_TOPIC}",serialized_product)
+    product_list_proto = await consume_message_response_get_all()
+
+    product_list = [
+        {
+            "id":product.id,
+            "product_id":str(product.product_id),
+            "name":product.name,
+            "description":product.description,
+            "price":product.price,
+            "is_available":product.is_available,
+
+        }
+        for product in product_list_proto.products
+    ]
+    return product_list
+
+
+
+@app.get("/products/{product_id}", response_model=dict)
+async def get_a_product(product_id:UUID, producer:Annotated[AIOKafkaProducer,Depends(produce_message)]):
+    product_proto = product_pb2.Product(product_id =str(product_id),  option = product_pb2.SelectOption.GET)
+    serialized_product = product_proto.SerializeToString()
+    await producer.send_and_wait(f"{settings.KAFKA_TOPIC}",serialized_product)
+    product_proto = await consume_message_response_get()
+    if product_proto.error_message or product_proto.status :
+        raise HTTPException(status_code=product_proto.status, detail=product_proto.error_message)
+    else:
+        return MessageToDict(product_proto)
+
+
+@app.post("/products", response_model=dict)
+async  def add_product (product:Product , producer:Annotated[AIOKafkaProducer,Depends(produce_message)]):
+    product_proto = product_pb2.Product(name = product.name, description = product.description , price = product.price , is_available = product.is_available, option = product_pb2.SelectOption.CREATE)
+    serialized_product = product_proto.SerializeToString()
+    await producer.send_and_wait(f"{settings.KAFKA_TOPIC}",serialized_product)
+
+    return {f"product with name : {product.name} " : "added" }
+#  Kafka ka topic me jo bhi data bhaja ga wo hamesha binary me jai ga 
+#   Note : Serialization jasa marzi(json ya through protobuf ) karain lakin kafka ka broker me hamesha data binary form me jata ah 
+#  jab json format me data bhaj raha han to wo binary me convert karna parta ha like b"hi"
+#  lakin protobuf already searialize binary me karta ha is lia b likhna ke zarorat hi nahi ha 
+# Topic data accept hi binary me karta ha 
+#  json ka size zyada hota ha aur protobuf ka size kam hota ha  
+
+#  operation type in protobuf. yeh is lia kia ha ku kah consumer ko to nahi pta kah aus na kya task perform karna ha 
+#  ta ka tomic me clear ho kah yeh cheez create, update ya del ho rahi ha 
+
+@app.put("/products/{product_id}", response_model = dict)
+async  def update_product (product_id:UUID, product:Product , producer:Annotated[AIOKafkaProducer,Depends(produce_message)]):
+    product_proto = product_pb2.Product(product_id= str(product_id), name = product.name, description = product.description ,price = product.price , is_available = product.is_available, option = product_pb2.SelectOption.UPDATE)
+    serialized_product = product_proto.SerializeToString()
+    await producer.send_and_wait(f"{settings.KAFKA_TOPIC}",serialized_product)
+    product_proto = await consume_message_response_get()
+    if product_proto.error_message or product_proto.status :
+        raise HTTPException(status_code=product_proto.status, detail=product_proto.error_message)
+    else:
+        return{"Updated Message": MessageToDict(product_proto)}
+
+
+@app.delete("/products/{product_id}", response_model=dict)
+async  def delete_product (product_id:UUID, producer:Annotated[AIOKafkaProducer,Depends(produce_message)]):
+    product_proto = product_pb2.Product(product_id= str(product_id), option = product_pb2.SelectOption.DELETE)
+    serialized_product = product_proto.SerializeToString()
+    await producer.send_and_wait(f"{settings.KAFKA_TOPIC}",serialized_product)
+    product_proto = await consume_message_response_get()
+    if product_proto.error_message or product_proto.status :
+        raise HTTPException(status_code=product_proto.status, detail=product_proto.error_message)
+    else:
+        return{"Updated Message": product_proto.error_message }
+
+
+
+    # return {f"product with product_id:{product_id}  ": {"Deleted"}}
+
+
+
